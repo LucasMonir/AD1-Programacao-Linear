d1fa48e6187da553154abf3d531e3962dd4ad1ea
Lucas <55569386+LucasMonir@users.noreply.github.com>
2020-11-09 19:17:01 -0300

    Versão final

---
+++ Trabalho/Aresta.java
@@ -0,0 +1,11 @@
+package Trabalho;
+
+public class Aresta {
+    int valor;
+    Vertice destino;
+
+    public Aresta(int valor, Vertice destino) {
+        this.valor = valor;
+        this.destino = destino;
+    }
+}
---
+++ Trabalho/Vertice.java
@@ -0,0 +1,26 @@
+package Trabalho;
+
+// implementa interface Comparable para comparação na priorityQueue
+public class Vertice implements Comparable<Vertice> {
+    // nome do vertice, arestas que saem dele, distancia (setada como maximo para aplicar o algoritimo),
+    // se ele foi visitado e o vertice anterior (como uma lista simples encadeada). 
+
+    int id;
+    Aresta[] adjacentes;
+    double distancia = Integer.MAX_VALUE;
+    boolean visitado;
+    Vertice anterior;
+
+    // construtor vazio para os vertices inicio e fim (no Main)
+    public Vertice() {}
+
+    // construtor com apenas o id, outros valores são adicionados durante processamento
+    public Vertice(int id) {
+        this.id = id;
+    }
+
+    // metodo da interface Comparable
+    public int compareTo(Vertice other) {
+        return Double.compare(distancia, other.distancia);
+    }
+}
